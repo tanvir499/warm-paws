6cb7f20d4804b26ed30e178e3bca26460d150247
Tanvir <126787226+tanvir499@users.noreply.github.com>
2025-11-21 06:20:00 +0600

    added extra part

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Slider from '../Component/Slider';
 import PopularSection from '../Component/PopularSection';
+import MeetOurVet from '../Component/MeetOurVet';
 
 const Home = () => {
     return (
         <div>
            <Slider></Slider>
            <PopularSection></PopularSection>
+           <MeetOurVet></MeetOurVet>
         </div>
     );
 };
